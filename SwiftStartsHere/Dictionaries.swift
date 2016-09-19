55a9505ab8878598b283375327737f4166e47c51
Devendar.reddy@emc.com <Devendar.reddy@emc.com>
2016-09-19 20:17:12 +0530

    Dictionaries Finishing Touch

--- SwiftStartsHere/Dictionaries.swift
+++ SwiftStartsHere/Dictionaries.swift
@@ -21,20 +21,23 @@ class Dictionaries: NSObject
      Replacing an Object with another
      Adding Key-Value pairs to a Dictionary
      Accessing all keys / values from Dictionary as Collection
-     
+     Remove all Pairs from Dictionary
      */
+    
   func playWithDictionaries()
   {
-    
+    // Creating Dictionaries
     let anEmptyDict = [String:String]()
     
-    var aDict: [String:String] = ["Key1":"Value1", "Key2": "Value2", "Key3" : "Value3"];
+    var aDict: [String:String] = ["Key1":"Value1", "Key2": "Value2", "Key3" : "Value3"]
+    
+//    var newDict: Dictionary<String : String> = ["Key1":"Value1", "Key2": "Value2", "Key3" : "Value3"] // Swift 3
     
     let bDict: [Int : String] = [1:"One", 2 : "Two"]
     
     print("Number of pairs in dict: \(aDict.count)")
     
-    // Overriding / adding new pair to a Dictiopnary
+    // Replaces / Adds New pair to a Dictiopnary
     aDict["Key4"] = "Value4";
     print("Elements in dict : \(aDict)");
     
@@ -43,7 +46,6 @@ class Dictionaries: NSObject
     
     // Overriding existing value for given key
     aDict.updateValue("Value44", forKey: "Key4");
-    
     print("Elements in dict : \(aDict)");
     
     // Finding all keys / values of Dictionary
@@ -55,6 +57,7 @@ class Dictionaries: NSObject
     {
         print("\(key) : \(value)");
     }
+    
     print("Value : \(aDict["Key1"])");
     
     // Comparing Two Dictionaries
@@ -65,7 +68,7 @@ class Dictionaries: NSObject
     
     // Removing a Key-Value pair from a Dictionary
     aDict.removeValueForKey("Key1");
-    
+    aDict["Key3"] = nil;
     // Removing all Objects from Dictionary
     aDict.removeAll()
     
